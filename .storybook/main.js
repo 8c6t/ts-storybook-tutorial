9479f36df1847730ea25435db661b2c66ce49e70
8c6t <nkw8620@gmail.com>
2020-06-15 00:32:25 +0900

    Storybook을 다양한 Addon과 함께 활용해보면서 사용법 정복하기

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,9 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  stories: ["../src/**/*.stories.(js|mdx)"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-knobs",
+    "@storybook/addon-docs",
+  ],
 };
